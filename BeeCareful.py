c98310932e732b19be8c7d6de41d53d4c4fed23f
AlexandreCAPY <alexandre.capy@gmail.com>
2023-12-09 22:45:40 +0100

    j'ai au bout d'un moment arété

--- BeeCareful.py
+++ BeeCareful.py
@@ -49,7 +49,7 @@ def main():
     # Creating bees
     bees = []
 
-    num_bees = 50
+    num_bees = 2
 
     for _ in range(num_bees):
         x, y = random.randint(0, grid_width - 1), random.randint(0, grid_height - 1)
