9be8ef96eba4f8ed0e247fbcfe367262a97245c7
besjb <bes.jeanbaptiste@gmail.com>
2023-12-09 15:41:07 +0100

    get_pollen et restaure functions

--- Plant.py
+++ Plant.py
@@ -9,8 +9,23 @@ class Plant:
         self.current_pollen = max_pollen
         self.cooldown = cooldown
         self.timer = cooldown
+        self.onCooldown = False
 
     def draw_plant(self, window, position, cell_width, cell_height, image):
         x = position[0] 
         y = position[1]  
         window.blit(image, (x, y))
+
+    def get_pollen(self):
+        speed = 1 # vitesse a laquelle le pollen est recupere
+        self.pollen -= speed
+        if self.pollen <= 0:
+            self.onCooldown = True
+        return speed # retourne le pollen pris
+
+    def restaure(self):
+        speed = 1 # vitesse a laquelle le pollen est restaure
+        self.pollen += speed
+        if self.pollen >= self.max_pollen:
+            self.pollen = max_pollen
+            self.onCooldown = False
